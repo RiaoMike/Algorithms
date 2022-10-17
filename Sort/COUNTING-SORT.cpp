7a6dad1d46691988e49cfc08af17ade4cbd8f65d
Riao Mike <xuruimaths@qq.com>
2022-10-17 12:19:39 +0800

    counting-sort

---
+++ Sort/COUNTING-SORT.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+// input array: A
+// output array: B
+// n: the length of A and B
+// the entries of A is range from 0 to k
+void CountingSort(int A[], int B[], int n){
+	// compute the k, which is the maximal element of A
+	int k = A[0];
+	for (int i = 1; i < n; ++i){
+		if (A[i] > k){
+			k = A[i];
+		}
+	}
+	// range from 0 to k **containing k**
+	k = k + 1;
+	int *C = new int[k];
+	// initialize
+	for (int i = 0; i < k; ++i){
+		C[i] = 0;
+	}
+	// C[i] contains the number of i 
+	for (int i = 0; i < n; ++i){
+		C[A[i]]	= C[A[i]] + 1;
+	}
+	// C[i] contains the number of elements less than or equal to i
+	for (int i = 1; i < k; ++i){
+		C[i] = C[i] + C[i-1];
+	}
+
+	for (int j = 0; j < n; ++j){
+		B[C[A[j]] - 1] = A[j];
+		C[A[j]] = C[A[j]] - 1;
+	}
+
+	delete [] C;
+}
+
+int main(){
+	// testing
+	int A[] = {2, 5, 3, 0, 2, 3, 0, 3};
+	int length = sizeof(A)/sizeof(A[0]);
+	int *B = new int[length];
+
+	CountingSort(A, B, length);
+
+	for (int i = 0; i < length; ++i){
+		cout << B[i] << " " ;
+	}
+	cout << endl;
+
+	delete []B;
+	return 0;
+}
